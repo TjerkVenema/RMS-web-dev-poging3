a886bc78a9116622b8e4f85967084c5a665aa4dd
TjerkVenema <71321579+TjerkVenema@users.noreply.github.com>
2021-04-12 16:31:29 +0200

    uitloggen

---
+++ RMS web dev poging3/Pages/Paginas/Uitloggen.cshtml.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RMS_web_dev_poging3.Pages.Paginas
+{
+    public class Uitloggen : PageModel
+    {
+        public RedirectToPageResult OnGet()
+        {
+            Response.Cookies.Delete("user_id");
+            return RedirectToPage("/Index");
+        }
+    }
+}
