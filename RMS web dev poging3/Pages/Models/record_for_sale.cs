67588c05617bba489674dac795b76af0d6a74eb1
Samuel <samueldjensen2003@gmail.com>
2021-04-14 11:18:36 +0200

    meer validatie

--- RMS web dev poging3/Pages/Models/record_for_sale.cs
+++ RMS web dev poging3/Pages/Models/record_for_sale.cs
@@ -1,10 +1,13 @@
-﻿namespace RMS_web_dev_poging3.Pages.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RMS_web_dev_poging3.Pages.Models
 {
     public class record_for_sale
     {
         public int recordForSaleId { get; set; }
         public int user_id { get; set; }
         public int record_id { get; set; }
+        [Range(1, 99999, ErrorMessage = "Voer een prijs in tussen €1,- en €99999,-")]
         public float priceSold { get; set; }
     }
 }
